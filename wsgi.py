3904b0b43040bb508321acf0f786fca949e55f96
Sergio Plaza <sergius2002@gmail.com>
2025-04-03 19:26:27 -0300

    feat: Agregadas variables de entorno de BCI al WSGI

--- wsgi.py
+++ wsgi.py
@@ -12,5 +12,11 @@ if app_path not in sys.path:
     sys.path.append(app_path)
 os.chdir(app_path)
 
+# Configuración de BCI
+os.environ['BCI_CLIENT_ID'] = 'XO1F0IQsd5iJHEA0pJTGmAQGAjSGa1k3'
+os.environ['BCI_CLIENT_SECRET'] = 'wmcgXFQYAHmnuNJm'
+os.environ['BCI_REDIRECT_URI'] = 'https://sancristobalspa.eu.pythonanywhere.com/bci/callback'
+os.environ['BCI_API_BASE_URL'] = 'https://apiprogram.bci.cl/sandbox/v1'
+
 # Importar la aplicación
 from app import app as application 
